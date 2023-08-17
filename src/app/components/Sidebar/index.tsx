7ad866b52b31309dbdf46c666b40ab8d27bc8ded
Renan <renan.fachin@hotmail.com>
2023-08-17 18:09:11 -0300

    Cria componente de usedSpaceWidget

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,10 +1,22 @@
 import { Logo } from './Logo'
-import { Search } from 'lucide-react'
-import { MainNavigation } from './MainNavigation'
+import {
+  Home,
+  BarChart,
+  SquareStack,
+  CheckSquare,
+  Flag,
+  Search,
+  Users,
+  LifeBuoy,
+  Cog,
+} from 'lucide-react'
+
+import { NavItem } from './NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -16,7 +28,21 @@ export function Sidebar() {
         />
       </div>
 
-      <MainNavigation />
+      <NavItem icon={Home} title="Home" />
+      <NavItem icon={BarChart} title="Dashboard" />
+      <NavItem icon={SquareStack} title="Projects" />
+      <NavItem icon={CheckSquare} title="Tasks" />
+      <NavItem icon={Flag} title="Reporting" />
+      <NavItem icon={Users} title="Users" />
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <UsedSpaceWidget />
+      </div>
     </aside>
   )
 }
