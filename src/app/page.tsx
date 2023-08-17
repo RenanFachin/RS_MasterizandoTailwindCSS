59dcdc5739ac34c413e51d45c91626bd604f5ebc
Renan <renan.fachin@hotmail.com>
2023-08-17 15:48:32 -0300

    Responsividade e breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="flex items-center gap-3 font-bold text-5xl before:w-0.5 before:h-8 before:bg-sky-500 before:flex">Hello Tailwind</h1>
+      <h1 className="flex items-center gap-3 font-bold text-3xl sm:text-5xl lg:text-6xl before:w-0.5 before:h-8 before:bg-sky-500 before:flex">Hello Tailwind</h1>
       <h1>Hello Tailwind</h1>
       <h1>Hello Tailwind</h1>
 
