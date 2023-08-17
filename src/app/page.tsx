c796ab4ca7a1492e2fedc66eaf2c634e4f67c7ef
Renan <renan.fachin@hotmail.com>
2023-08-17 15:42:33 -0300

    Seletores e Estados no tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl">Hello Tailwind</h1>
+      <h1 className="flex items-center gap-3 font-bold text-5xl before:w-0.5 before:h-8 before:bg-sky-500 before:flex">Hello Tailwind</h1>
       <h1>Hello Tailwind</h1>
       <h1>Hello Tailwind</h1>
+
+      <button
+        disabled
+        className="bg-sky-500 px-4 py-2 rounded-md font-semibold mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
+      >
+        Sign in
+      </button>
     </div>
   )
 }
