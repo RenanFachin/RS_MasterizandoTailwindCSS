d8300a11efbd3f11e4b608d7e75668961a9f33e8
Renan <renan.fachin@hotmail.com>
2023-08-18 20:00:45 -0300

    Cria componente fileItem e utiliza SLOTS API do tailwind variants para aplicar customizações personalizadas conforme o status do progresso de upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,40 +164,25 @@ export default function Home() {
                 </SelectInput>
 
                 <div className="flex items-center gap-1">
-                  <button
-                    type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                  >
+                  <Button type="button" variant="ghost">
                     <Bold className="h-4 w-4 text-zinc-500" />
-                  </button>
+                  </Button>
 
-                  <button
-                    type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                  >
+                  <Button type="button" variant="ghost">
                     <Italic className="h-4 w-4 text-zinc-500" />
-                  </button>
+                  </Button>
 
-                  <button
-                    type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                  >
+                  <Button type="button" variant="ghost">
                     <Link className="h-4 w-4 text-zinc-500" />
-                  </button>
+                  </Button>
 
-                  <button
-                    type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                  >
+                  <Button type="button" variant="ghost">
                     <List className="h-4 w-4 text-zinc-500" />
-                  </button>
+                  </Button>
 
-                  <button
-                    type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                  >
+                  <Button type="button" variant="ghost">
                     <ListOrdered className="h-4 w-4 text-zinc-500" />
-                  </button>
+                  </Button>
                 </div>
               </div>
 
