4591ff0c030231d7d750e2315717d1122c4a2a44
Renan <renan.fachin@hotmail.com>
2023-08-18 18:13:41 -0300

    Componentiza o componente SelectInput e cria input de textarea

--- src/app/components/SelectInput/index.tsx
+++ src/app/components/SelectInput/index.tsx
@@ -1,15 +1,22 @@
 'use client'
 import * as Select from '@radix-ui/react-select'
-import { Check, ChevronDown } from 'lucide-react'
+import { ChevronDown } from 'lucide-react'
+import { ReactNode } from 'react'
 
-export function SelectInput() {
+export interface SelectInputProps extends Select.SelectProps {
+  children: ReactNode
+  placeholder: string
+}
+
+export function SelectInput({
+  children,
+  placeholder,
+  ...props
+}: SelectInputProps) {
   return (
-    <Select.Root>
+    <Select.Root {...props}>
       <Select.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placegholder]:text-zinc-600">
-        <Select.Value
-          className="text-black"
-          placeholder="Select a country..."
-        />
+        <Select.Value className="text-black" placeholder={placeholder} />
         <Select.Icon>
           <ChevronDown className="h-5 w-5 text-zinc-500" />
         </Select.Icon>
@@ -22,33 +29,11 @@ export function SelectInput() {
           position="popper"
           sideOffset={10}
           // w-[--radix-select-trigger-width] faz com que a width seja exatamente do tamanho do trigger, ou seja, do input
-          className="z-10 w-[--radix-select-trigger-width] rounded-lg border border-zinc-300 bg-white"
+          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-300 bg-white shadow-sm"
         >
           <Select.Viewport>
             {/* data-[highlighted]:bg-zinc-50 é um data attribute do componente do radix para mostrar qual item o usuário está fazendo um hover */}
-            <Select.Item
-              value="br"
-              className="flex items-center justify-between gap-2 px-3 py-2.5 outline-none data-[highlighted]:bg-zinc-50"
-            >
-              <Select.ItemText className="text-black">Brasil</Select.ItemText>
-
-              <Select.ItemIndicator>
-                <Check className="h-4 w-4 text-violet-500" />
-              </Select.ItemIndicator>
-            </Select.Item>
-
-            <Select.Item
-              value="us"
-              className="flex items-center justify-between gap-2 px-3 py-2.5 outline-none data-[highlighted]:bg-zinc-50"
-            >
-              <Select.ItemText className="text-black">
-                United States
-              </Select.ItemText>
-
-              <Select.ItemIndicator>
-                <Check className="h-4 w-4 text-violet-500" />
-              </Select.ItemIndicator>
-            </Select.Item>
+            {children}
           </Select.Viewport>
         </Select.Content>
       </Select.Portal>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import * as FileInput from './components/FileInput'
 
 import { SettingTabs } from './components/SettingsTabs'
 
-import { Mail } from 'lucide-react'
+import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 import { SelectInput } from './components/SelectInput'
+import { SelectItem } from './components/SelectInput/SelectItem'
+import { Textarea } from './components/Textarea'
 
 export default function Home() {
   return (
@@ -127,7 +129,9 @@ export default function Home() {
               Country
             </label>
 
-            <SelectInput />
+            <SelectInput placeholder="Select a country...">
+              <SelectItem value="br" text="Brazil" />
+            </SelectInput>
           </div>
 
           {/* Timezone - select */}
@@ -139,7 +143,14 @@ export default function Home() {
               Timezone
             </label>
 
-            <div></div>
+            <SelectInput placeholder="Select a timezone...">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (PST) (UTC−08:00)"
+              />
+
+              <SelectItem value="utc3" text="America São Paulo (UTC−03:00)" />
+            </SelectInput>
           </div>
 
           {/* bio - textarea + */}
@@ -151,7 +162,56 @@ export default function Home() {
               </span>
             </label>
 
-            <div></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-2 gap-3">
+                <SelectInput placeholder="" defaultValue="normal">
+                  <SelectItem value="normal" text="Normal Text" />
+                  <SelectItem value="md" text="markdown" />
+                </SelectInput>
+
+                <div className="flex items-center gap-1">
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Bold className="h-4 w-4 text-zinc-500" />
+                  </button>
+
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Italic className="h-4 w-4 text-zinc-500" />
+                  </button>
+
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Link className="h-4 w-4 text-zinc-500" />
+                  </button>
+
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <List className="h-4 w-4 text-zinc-500" />
+                  </button>
+
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <ListOrdered className="h-4 w-4 text-zinc-500" />
+                  </button>
+                </div>
+              </div>
+
+              <Textarea
+                id="bio"
+                defaultValue="I'm a Product Designer based in Melbourne, Australia. I specialise in UX/UI design, brand strategy, and Webflow development."
+              />
+            </div>
           </div>
 
           {/* portfolio - upload + */}
