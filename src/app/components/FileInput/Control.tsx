d1f460c5c809765ab4598b43469d03d2a30859c0
Renan <renan.fachin@hotmail.com>
2023-08-18 15:54:47 -0300

    feat: Fazendo a atualização do state a partir do contexto dos componentes de fileInput e adicionando uma preview da imagem enviada pelo user

--- src/app/components/FileInput/Control.tsx
+++ src/app/components/FileInput/Control.tsx
@@ -1,12 +1,23 @@
 'use client'
-import { ComponentProps } from 'react'
+import { ChangeEvent, ComponentProps } from 'react'
 import { useFileInput } from './Root'
 
 type ControlProps = ComponentProps<'input'>
 
 export function Control(props: ControlProps) {
   // capturando as informações do contexto
-  const { id } = useFileInput()
+  const { id, onFilesSelected } = useFileInput()
+
+  function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files?.length) {
+      return
+    }
+
+    // Convertendo para um array a lista de files enviados pelo evento de onchange
+    const files = Array.from(event.target.files)
+
+    onFilesSelected(files)
+  }
 
   return (
     <input
@@ -14,6 +25,7 @@ export function Control(props: ControlProps) {
       // sr-only é um hack para deixar invísível porém existente para os leitores de tela e com isso, podemos estilizar apenas a LABEL e não o input
       className="sr-only"
       id={id}
+      onChange={handleFilesSelected}
       {...props}
     />
   )
