493f12da26157cd67b955a76711cbbdcca7e3577
Renan <renan.fachin@hotmail.com>
2023-08-20 21:18:08 -0300

    Tema dark na sidebar

--- src/app/components/Input/index.tsx
+++ src/app/components/Input/index.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 // Começo do input
 type InputPrefixProps = ComponentProps<'div'>
@@ -11,7 +12,7 @@ type InputControlProps = ComponentProps<'input'>
 function InputControl(props: InputControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+      className="outline-non flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 dark:text-zinc-100 dark:placeholder-zinc-400"
       {...props}
     />
   )
@@ -22,7 +23,12 @@ type InputRoot = ComponentProps<'div'>
 function InputRoot(props: InputRoot) {
   return (
     <div
-      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-400 focus-within:ring-2 focus-within:ring-violet-100"
+      className={twMerge(
+        'flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm',
+        'focus-within:border-violet-400 focus-within:ring-2 focus-within:ring-violet-100',
+        'dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-b-violet-500 dark:focus-within:ring-violet-500/20',
+        props.className,
+      )}
       {...props}
     />
   )
--- src/app/components/Sidebar/Profile.tsx
+++ src/app/components/Sidebar/Profile.tsx
@@ -11,10 +11,10 @@ export function Profile() {
       />
 
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Renan Fachin
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           renan.fachin@hotmail.com
         </span>
       </div>
